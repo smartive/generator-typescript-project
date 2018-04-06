eeabc7cc4ae9116d6db7aeb2670749e84b8eea8c
Christoph Bühler <christoph@smartive.ch>
2018-04-06 17:35:03 +0200

    test :/

--- test/generator.spec.js
+++ test/generator.spec.js
@@ -1,26 +1,27 @@
-// const { join } = require('path');
-// const helpers = require('yeoman-test');
-// const TypescriptProjectGenerator = require('../app');
+const { join } = require('path');
+const helpers = require('yeoman-test');
+const TypescriptProjectGenerator = require('../app');
 
-// const defaultPrompts = {
-//     name: 'name',
-//     description: 'description',
-//     type: 'library',
-//     ciconfigs: [],
-//     withTypedoc: false,
-//     initializeGitRepo: false,
-// }
+const defaultPrompts = {
+    name: 'name',
+    description: 'description',
+    type: 'library',
+    ciconfigs: [],
+    withTypedoc: false,
+    initializeGitRepo: false,
+}
 
 // const setup = helpers.setUpTestDirectory(join(__dirname, './temp'));
 
-// describe('typescript project generator', () => {
+describe('typescript project generator', () => {
 
-//     beforeEach(() => {
-//     });
+    beforeEach(() => {
+    });
 
-//     it('sdf', async () => {
-//         const a = await helpers.run(TypescriptProjectGenerator);
-//         console.log(a);
-//     });
+    it('should be tested', async () => {
+        expect(true).toBe(true);
+        // const a = await helpers.run(TypescriptProjectGenerator);
+        // console.log(a);
+    });
 
-// });
+});
