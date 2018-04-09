56b81cf958ad37fa09de98ab8e6a1075399519a2
Christoph Bühler <christoph@smartive.ch>
2018-04-09 12:12:45 +0200

    adding file tests

--- test/generator.spec.js
+++ test/generator.spec.js
@@ -114,41 +114,189 @@ describe('typescript project generator', () => {
             expect(content).toMatchSnapshot();
         });
 
-        it('should change main in package json file for application type');
+        it('should change main in package json file for application type', async () => {
+            const folder = await helpers
+                .run(TypescriptProjectGenerator)
+                .withPrompts({
+                    ...defaultPrompts,
+                    type: 'application',
+                });
+            const content = readFileSync(join(folder, 'name', 'package.json'), 'utf8');
+            expect(content).toMatchSnapshot();
+        });
 
-        it('should add typedoc to package json');
+        it('should add typedoc to package json', async () => {
+            const folder = await helpers
+                .run(TypescriptProjectGenerator)
+                .withPrompts({
+                    ...defaultPrompts,
+                    type: 'application',
+                    withTypedoc: true
+                });
+            const content = readFileSync(join(folder, 'name', 'package.json'), 'utf8');
+            expect(content).toMatchSnapshot();
+        });
 
-        it('should add giturl to package json');
+        it('should add giturl to package json', async () => {
+            const folder = await helpers
+                .run(TypescriptProjectGenerator)
+                .withPrompts({
+                    ...defaultPrompts,
+                    initializeGitRepo: true,
+                    gitUrl: 'https://github.com/smartive/generator-typescript-project',
+                });
+            const content = readFileSync(join(folder, 'name', 'package.json'), 'utf8');
+            expect(content).toMatchSnapshot();
+        });
 
-        it('should add access config to package json if package is scoped');
+        it('should add access config to package json if package is scoped', async () => {
+            const folder = await helpers
+                .run(TypescriptProjectGenerator)
+                .withPrompts({
+                    ...defaultPrompts,
+                    name: '@smartive/scoped-name'
+                });
+            const content = readFileSync(join(folder, 'smartive_scoped-name', 'package.json'), 'utf8');
+            expect(content).toMatchSnapshot();
+        });
 
-        it('should add default tsconfig');
+        it('should add default tsconfig', async () => {
+            const folder = await helpers
+                .run(TypescriptProjectGenerator)
+                .withPrompts(defaultPrompts);
+            const content = readFileSync(join(folder, 'name', 'tsconfig.json'), 'utf8');
+            expect(content).toMatchSnapshot();
+        });
 
-        it('should add default ts base config');
+        it('should add default ts base config', async () => {
+            const folder = await helpers
+                .run(TypescriptProjectGenerator)
+                .withPrompts(defaultPrompts);
+            const content = readFileSync(join(folder, 'name', 'config', 'tsconfig.base.json'), 'utf8');
+            expect(content).toMatchSnapshot();
+        });
 
-        it('should add default ts build config');
+        it('should add default ts build config', async () => {
+            const folder = await helpers
+                .run(TypescriptProjectGenerator)
+                .withPrompts(defaultPrompts);
+            const content = readFileSync(join(folder, 'name', 'config', 'tsconfig.build.json'), 'utf8');
+            expect(content).toMatchSnapshot();
+        });
 
-        it('should change out dire in ts build config for application');
+        it('should change out dir in ts build config for application', async () => {
+            const folder = await helpers
+                .run(TypescriptProjectGenerator)
+                .withPrompts({
+                    ...defaultPrompts,
+                    type: 'application',
+                });
+            const content = readFileSync(join(folder, 'name', 'config', 'tsconfig.build.json'), 'utf8');
+            expect(content).toMatchSnapshot();
+        });
 
-        it('should remove comments in build config for application');
+        it('should remove comments and declarations in build config for application', async () => {
+            const folder = await helpers
+                .run(TypescriptProjectGenerator)
+                .withPrompts({
+                    ...defaultPrompts,
+                    type: 'application',
+                });
+            const content = readFileSync(join(folder, 'name', 'config', 'tsconfig.base.json'), 'utf8');
+            expect(content).toMatchSnapshot();
+        });
 
-        it('should remove declarations in build config for application');
+        it('should add dom lib in build config with typedoc', async () => {
+            const folder = await helpers
+                .run(TypescriptProjectGenerator)
+                .withPrompts({
+                    ...defaultPrompts,
+                    withTypedoc: true,
+                });
+            const content = readFileSync(join(folder, 'name', 'config', 'tsconfig.base.json'), 'utf8');
+            expect(content).toMatchSnapshot();
+        });
 
-        it('should add dom lib in build config with typedoc');
+        it('should add default tslint config', async () => {
+            const folder = await helpers
+                .run(TypescriptProjectGenerator)
+                .withPrompts(defaultPrompts);
+            const content = readFileSync(join(folder, 'name', 'tslint.json'), 'utf8');
+            expect(content).toMatchSnapshot();
+        });
 
-        it('should add default tslint config');
+        it('should add npmrc for library', async () => {
+            const folder = await helpers
+                .run(TypescriptProjectGenerator)
+                .withPrompts(defaultPrompts);
+            expect(existsSync(join(folder, 'name', '.npmrc'))).toBe(true);
+        });
 
-        it('should add npmrc for library');
+        it('should not add npmrc for application', async () => {
+            const folder = await helpers
+                .run(TypescriptProjectGenerator)
+                .withPrompts({
+                    ...defaultPrompts,
+                    type: 'application',
+                });
+                expect(existsSync(join(folder, 'name', '.npmrc'))).toBe(false);
+        });
 
-        it('should add gitlab ci if selected');
+        it('should add gitlab ci if selected', async () => {
+            const folder = await helpers
+                .run(TypescriptProjectGenerator)
+                .withPrompts({
+                    ...defaultPrompts,
+                    ciconfigs: ['GitLab'],
+                });
+            const content = readFileSync(join(folder, 'name', '.gitlab-ci.yml'), 'utf8');
+            expect(content).toMatchSnapshot();
+        });
 
-        it('should add appveyor ci if selected');
+        it('should add appveyor ci if selected', async () => {
+            const folder = await helpers
+                .run(TypescriptProjectGenerator)
+                .withPrompts({
+                    ...defaultPrompts,
+                    ciconfigs: ['Appveyor'],
+                });
+            const content = readFileSync(join(folder, 'name', '.appveyor.yml'), 'utf8');
+            expect(content).toMatchSnapshot();
+        });
 
-        it('should add travis ci if selected');
+        it('should add travis ci if selected', async () => {
+            const folder = await helpers
+                .run(TypescriptProjectGenerator)
+                .withPrompts({
+                    ...defaultPrompts,
+                    ciconfigs: ['Travis'],
+                });
+            const content = readFileSync(join(folder, 'name', '.travis.yml'), 'utf8');
+            expect(content).toMatchSnapshot();
+        });
 
-        it('should name file index.ts if its a library');
+        it('should name file index.ts if its a library', async () => {
+            const folder = await helpers
+                .run(TypescriptProjectGenerator)
+                .withPrompts(defaultPrompts);
+            expect(existsSync(join(folder, 'name', 'src', 'index.ts'))).toBe(true);
+            expect(existsSync(join(folder, 'name', 'src', 'app.ts'))).toBe(false);
+            expect(existsSync(join(folder, 'name', 'test', 'index.spec.ts'))).toBe(true);
+            expect(existsSync(join(folder, 'name', 'test', 'app.spec.ts'))).toBe(false);
+        });
 
-        it('should name file app.ts if its an application');
+        it('should name file app.ts if its an application', async () => {
+            const folder = await helpers
+                .run(TypescriptProjectGenerator)
+                .withPrompts({
+                    ...defaultPrompts,
+                    type: 'application',
+                });
+            expect(existsSync(join(folder, 'name', 'src', 'app.ts'))).toBe(true);
+            expect(existsSync(join(folder, 'name', 'src', 'index.ts'))).toBe(false);
+            expect(existsSync(join(folder, 'name', 'test', 'app.spec.ts'))).toBe(true);
+            expect(existsSync(join(folder, 'name', 'test', 'index.spec.ts'))).toBe(false);
+        });
 
     });
 
